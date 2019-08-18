05d99b00536963b5d3c2589b6d595b061d82b6e2
unknown <ajayfranciss896@gmail.com>
2019-08-18 21:11:07 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ajay-app';
+
+//   onsubmit(length,qty,name){
+// console.log("submit")
+// console.log(name)
+//     console.log(length,qty)
+//   }
+}
